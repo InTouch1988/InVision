1fe8e762a691dc6be788e3c04e9c9ec7c3e016df
InTouch1988 <roman.bzhezitskiy@gmail.com>
2023-02-06 18:48:58 +0200

    api tests_2

--- test/specs/APItest.spec.ts
+++ test/specs/APItest.spec.ts
@@ -23,33 +23,44 @@ describe('Trello API tests', async ()=> {
         const response = await api.post('1/boards');
         ids.boardId = await response.data.id;
         console.log(await ids.boardId);
+        await expect(response.status).toEqual(200);
+        await expect(response.data.closed).toBeFalsy();
     });
 
     it('Create TODO list', async() => {
         const response = api.post('1/lists', undefined, { params: { idBoard: ids.boardId, name: 'TODO'}});
         ids.toDoListId = await (await response).data.id;
+        await expect((await response).status).toEqual(200);
+        await expect((await response).data.name).toEqual('TODO');
     });
 
     it('Create DONE list', async() => {
         const response = api.post('1/lists', undefined, { params: { idBoard: ids.boardId, name: 'DONE'}});
         ids.doneListId = await (await response).data.id;
+        await expect((await response).status).toEqual(200);
+        await expect((await response).data.name).toEqual('DONE');
     });
 
     it('Create a card in TODO list', async() => {
         const response = api.post('1/cards', undefined, { params: { idList: ids.toDoListId, name: 'Some new card'}});
         ids.cardId = await (await response).data.id;
+        await expect((await response).status).toEqual(200);
+        await expect((await response).data.name).toEqual('Some new card');
     });
 
     it('Move a card to DONE list', async() => {
         const response = api.put(`1/cards/${ids.cardId}`, undefined, { params: { idList: ids.doneListId}});
+        await expect((await response).status).toEqual(200);
     });
 
     it('Get all boards', async () => {
         const response = await api.get('1/members/me/boards');
+        await expect((await response).status).toEqual(200);
     });
 
     it('Delete a board', async() => {
         const response = await api.delete(`1/boards/${ids.boardId}`);
-        console.log(await response);
+        await expect((await response).status).toEqual(200);
+        await expect(response.data["_value"]).toBeNull();
     });
 });
